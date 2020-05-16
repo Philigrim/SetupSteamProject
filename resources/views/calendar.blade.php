9a98e591eb43f127738260ed1b200d0019de75d8
Nedas Jankauskas <nedas.jankauskas@mif.stud.vu.lt>
2020-05-16 22:43:18 +0300

    kalendorius

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -20,69 +20,78 @@
         </div>
     </div>
 </div>
-<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.css' />
 <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-<script src='https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.17.1/moment.min.js'></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.9.0/fullcalendar.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.1.0/locale/lt.js"></script>
+
 
 
 <script>
 
 
-            // page is now ready, initialize the calendar...
-        $('#calendar').fullCalendar({
-            displayEventEnd:true,
-            eventColor: '#356ACC',
-            eventTextColor: '#fff',
-            timeFormat: 'h:mm', // uppercase H for 24-hour clock
-            header: {
-            center: 'month,agendaWeek' // buttons for switching between views
-            },
-            // put your options and callbacks here
-            events : [
-                @foreach($reservations as $reservation)
-                {
-                    title : ' {{ $reservation->event->name }}',
-                    start : ' {{ $reservation->date.' '.$reservation->start_time }}',
-                    end: ' {{ $reservation->date.' '.$reservation->end_time }}',
-                    center: ' STEAM centras: {{ $reservation->event->room->steam->steam_name}}',
-                    room: ' Kambarys: {{ $reservation->event->room->room_number}}',
-                    city: ' Miestas: {{ $reservation->event->room->steam->city->city_name}}'
-                },
-                @endforeach
-            ],
-            eventRender: function(event, element, view) {
-                var limit = 27;       
-                 element.find('.fc-title')[0].insertAdjacentHTML('beforebegin', "<br>");
+
+$('#calendar').fullCalendar({
+    // put your options and callbacks here
+  })
 
 
-},
 
-
-        eventMouseover: function(calEvent, jsEvent) {
-                
-    var tooltip = '<div class="tooltipevent" style="border-radius: 10px; background:#ccc;position:absolute;z-index:10001;">' + '<b>' + calEvent.title +'</b>' +'<br>'+ calEvent.city + '<br>'+ calEvent.center  +'<br>'+ calEvent.room+'</div>';
-    var $tooltip = $(tooltip).appendTo('body');
-
-    $(this).mouseover(function(e) {
-        $(this).css('z-index', 10000);
-        $tooltip.fadeIn('500');
-        $tooltip.fadeTo('10', 1.9);
-    }).mousemove(function(e) {
-        $tooltip.css('top', e.pageY + 10);
-        $tooltip.css('left', e.pageX + 20);
-    });
-},
-
-eventMouseout: function(calEvent, jsEvent) {
-    $(this).css('z-index', 8);
-    $('.tooltipevent').remove();
-},
+//             // page is now ready, initialize the calendar...
+//         $('#calendar').fullCalendar({
             
-        })
+//             displayEventEnd:true,
+//             googleCalendarApiKey: 'AIzaSyBDhlu0Xk18HA1UyGG7XghD-pFtECQbxxc',
+//             eventColor: '#356ACC',
+//             eventTextColor: '#fff',
+//             timeFormat: 'h:mm', // uppercase H for 24-hour clock
+//             header: {
+//             center: 'month,agendaWeek' // buttons for switching between views
+//             },
+//             // put your options and callbacks here
+//             events : [
+//                 @foreach($reservations as $reservation)
+//                 {
+//                     googleCalendarId: 'kgvlaiasuvtsfkp4h6r1na53co@group.calendar.google.com',
+//                     className: 'gcal-event',
+//                     title : ' {{ $reservation->event->name }}',
+//                     start : ' {{ $reservation->date.' '.$reservation->start_time }}',
+//                     end: ' {{ $reservation->date.' '.$reservation->end_time }}',
+//                     center: ' STEAM centras: {{ $reservation->event->room->steam->steam_name}}',
+//                     room: ' Kambarys: {{ $reservation->event->room->room_number}}',
+//                     city: ' Miestas: {{ $reservation->event->room->steam->city->city_name}}'
+//                 },
+//                 @endforeach
+//             ],
+//             eventRender: function(event, element, view) {
+//                 var limit = 27;       
+//                  element.find('.fc-title')[0].insertAdjacentHTML('beforebegin', "<br>");
+
+
+// },
+
+
+//         eventMouseover: function(calEvent, jsEvent) {
+                
+//     var tooltip = '<div class="tooltipevent" style="border-radius: 10px; background:#ccc;position:absolute;z-index:10001;">' + '<b>' + calEvent.title +'</b>' +'<br>'+ calEvent.city + '<br>'+ calEvent.center  +'<br>'+ calEvent.room+'</div>';
+//     var $tooltip = $(tooltip).appendTo('body');
+//     // kgvlaiasuvtsfkp4h6r1na53co@group.calendar.google.com
+//     // AIzaSyBDhlu0Xk18HA1UyGG7XghD-pFtECQbxxc
+//     $(this).mouseover(function(e) {
+//         $(this).css('z-index', 10000);
+//         $tooltip.fadeIn('500');
+//         $tooltip.fadeTo('10', 1.9);
+//     }).mousemove(function(e) {
+//         $tooltip.css('top', e.pageY + 10);
+//         $tooltip.css('left', e.pageX + 20);
+//     });
+// },
+
+// eventMouseout: function(calEvent, jsEvent) {
+//     $(this).css('z-index', 8);
+//     $('.tooltipevent').remove();
+// },
+            
+//         })
         
 
-</script>
+// </script>
 
 @endsection
